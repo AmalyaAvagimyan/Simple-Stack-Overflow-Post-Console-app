ad96bafd6d80d4caa7c68a06acb77108b2aeafb9
Amalya Avagimyan <amalya_avagimyan16@alumni.aua.am>
2021-02-06 21:46:02 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace StackOverFlowPost
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var post = new StOvPost("test title", "test description");
+
+            Console.WriteLine(@"Type 'upvote'or 'downvote' the post or 'quit' to end the program ");
+            var input = Console.ReadLine().ToLower();
+
+            while(true)
+            {
+                if (input == "upvote")
+                {
+                    post.UpVote();
+                    Console.WriteLine(@"Type 'upvote'or 'downvote' the post or 'quit' to end the program ");
+                    input = Console.ReadLine().ToLower();
+                }
+                else if (input == "downvote")
+                {
+                    post.DownVote();
+                    Console.WriteLine(@"Type 'upvote'or 'downvote' the post or 'quit' to end the program ");
+                    input = Console.ReadLine().ToLower();
+                }
+                else if (input == "quit")
+                {
+                    Console.WriteLine("The post has {0} total votes", post.CurrentUpVotes + post.CurrentDownVotes);
+                    Console.WriteLine("The post has {0} upvotes", post.CurrentUpVotes);
+                    Console.WriteLine("The post has {0} downvotes", post.CurrentDownVotes);
+                    Console.ReadLine();
+                    break;
+                }
+            }
+
+        }
+    }
+}
---
+++ StOvPost.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace StackOverFlowPost
+
+{
+    public class StOvPost
+    {
+        public int CurrentUpVotes { get; set; }
+        public int CurrentDownVotes { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Date 
+        {
+            get
+            { return DateTime.Now;}
+        }
+
+        public StOvPost()
+        {
+
+        }
+
+        public StOvPost(string title)
+        {
+            this.Title = title;
+          
+        }
+        public StOvPost(string title, string description)
+            : this(title)
+
+        {
+            this.Description = description;
+        }
+
+        public int UpVote()
+        {
+            return CurrentUpVotes += 1;
+           
+        }
+
+        public int DownVote()
+        {
+            return CurrentDownVotes += 1;
+           
+        }
+
+    }
+}
